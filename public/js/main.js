2cbe434122f525911d4e037dfa5c8a77ac6f0937
BLooDBRothER <computertechnology446@gmail.com>
2021-08-15 10:45:23 +0530

    First commit

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+import { test } from "../modules/util.js";
+
+console.log(test)
+
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/8.9.1/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="https://www.gstatic.com/firebasejs/8.9.1/firebase-analytics.js"></script>
+
+// <script>
+//   // Your web app's Firebase configuration
+//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyD6KZ3tVToX8Kc8jwM5afTzQInjaH9DtBE",
+//     authDomain: "tic-tac-toe-44.firebaseapp.com",
+//     projectId: "tic-tac-toe-44",
+//     storageBucket: "tic-tac-toe-44.appspot.com",
+//     messagingSenderId: "39983334316",
+//     appId: "1:39983334316:web:d656e83ed26fb656cab009",
+//     measurementId: "G-R6V8RKEB7Q"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+// </script>
